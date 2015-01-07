a54218c66f45786e70caf45e1dfd772a92afe863
João Pedro Nardari dos Santos <joaonardari@gmail.com>
2015-01-07 17:16:25 -0200

    1047

--- Category1/1047.cpp
+++ Category1/1047.cpp
@@ -6,8 +6,20 @@ int main() {
     int startHour,startMinute,endHour,endMinute;
     cin >> startHour >> startMinute >> endHour >> endMinute;
 
-    int hours, minutes;
-    if (startHour > endHour) {
-        // TODO
+    int totalMinutes,startTotalMinutes,endTotalMinutes;
+
+    startTotalMinutes = startHour * 60 + startMinute;
+    endTotalMinutes = endHour * 60 + endMinute;
+
+    totalMinutes = endTotalMinutes - startTotalMinutes;
+
+    if (startTotalMinutes<=endTotalMinutes)
+            if (totalMinutes == 0)
+                cout << "O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)" << endl;
+            else
+                cout << "O JOGO DUROU " << (totalMinutes-(totalMinutes%60))/60 << " HORA(S) E " << totalMinutes%60 << " MINUTO(S)" << endl;
+    else {
+        totalMinutes = (24*60 - startTotalMinutes) + endTotalMinutes;
+        cout << "O JOGO DUROU " << (totalMinutes-(totalMinutes%60))/ 60 << " HORA(S) E " << (totalMinutes%60) << " MINUTO(S)" << endl;
     }
 }
