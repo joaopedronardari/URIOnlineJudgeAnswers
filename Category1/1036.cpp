4f6008a02d5464ead94db8cd03477a411276bd40
João Pedro Nardari dos Santos <joao.nardari.santos@usp.br>
2015-01-08 09:56:16 -0200

    1036

--- Category1/1036.cpp
+++ Category1/1036.cpp
@@ -6,20 +6,18 @@ using namespace std;
 
 int main() {
     double a,b,c;
-    cin >> a >> b >> c;
 
-    double r1,r2;
-    double delta = pow(b,2) - (4*a*c);
-    if (delta < 0) {
-        cout << "Impossivel calcular" << endl;
-        return 0;
+    while (cin >> a >> b >> c) {
+        double delta = pow(b,2) - (4 * a * c);
+
+        if (delta < 0 || 2*a == 0) {
+            cout << "Impossivel calcular" << endl;
+        } else {
+            cout << fixed << setprecision(5);
+            cout << "R1 = " << (-b + (sqrt(delta))) / (2 * a) << endl;
+            cout << "R2 = " << (-b - (sqrt(delta))) / (2 * a) << endl;
+        }
     }
 
-    r1 = (-b + sqrt(delta)) / (2 * a);
-    r2 = (-b - sqrt(delta)) / (2 * a);
-
-    cout << fixed << setprecision(5);
-    cout << "R1 = " << r1 << endl;
-    cout << "R2 = " << r2 << endl;
     return 0;
 }
