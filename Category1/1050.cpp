c1d2a78df6b4dfb185417c9457ff8bc086b8e013
João Pedro Nardari dos Santos <joao.nardari.santos@usp.br>
2015-01-08 10:04:29 -0200

    1050

--- Category1/1050.cpp
+++ Category1/1050.cpp
@@ -3,6 +3,40 @@
 using namespace std;
 
 int main() {
+    int ddd;
+
+    while (cin >> ddd) {
+        switch (ddd) {
+        case 61:
+                cout << "Brasilia";
+            break;
+        case 71:
+                cout << "Salvador";
+            break;
+        case 11:
+                cout << "Sao Paulo";
+            break;
+        case 21:
+                cout << "Rio de Janeiro";
+            break;
+        case 32:
+                cout << "Juiz de Fora";
+            break;
+        case 19:
+                cout << "Campinas";
+            break;
+        case 27:
+                cout << "Vitoria";
+            break;
+        case 31:
+                cout << "Belo Horizonte";
+            break;
+        default:
+                cout << "DDD nao cadastrado";
+        }
+
+        cout << endl;
+    }
 
     return 0;
 }
